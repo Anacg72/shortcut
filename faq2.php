2015f741239fefe2a7ee1ec307258dec1bc8ece4
anacg72 <ana.garbeleche@gmail.com>
2018-03-18 17:06:21 -0300

    Hola chicos, subo la versión final de la sección "como funciona"? Por favor borren los archivos faq.php y faq.css porque los reemplacé por faq2.php y faq2.css
    Gracias! Ana

---
+++ faq2.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/faq2.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SHORCUT</title>
+</head>
+<body>
+	
+		<?php include "header.php" ?>
+
+	<div class="container">
+
+		<h1 class="titulomain"> ¿Cómo funciona? </h1>
+
+		<div class="main">
+			
+
+			<div class="textomain">
+
+				<h2> Busco Freelancers </h2>
+				<h3> 1. Publica tu proyecto </h3>
+				<p> Publicar tu proyecto siempre es gratis. Automáticamente comenzarás a recibir ofertas por parte de nuestros freelancers. De manera alternativa, puedes explorar el talento disponible en nuestro sitio y realizar, en lugar de ello, una oferta directa a un freelancer.</p> 
+
+
+				<h3>2. Seleccione freelancers</h3>
+				<ul>
+					<li> - Explora los perfiles de los freelancers. </li> <br>
+					<li> - Conversa en tiempo real</li><br>
+					<li> - Comparar propuestas </li><br>
+					<li> - Selecciona a tu favorito y hazle una oferta de trabajo.</li>
+				</ul>
+
+
+				<h3>3. ¡Acuerda la forma de trabajo y el pago! </h3>
+				<p> Conversa con el freelancer seleccionado sobre la forma en la que se pagará el trabajo y los entregables necesarios para que el pago </p> 
+
+			</div>
+
+			<div class="imgmain">
+
+				<img src="images/hola.jpg">
+
+			</div>
+
+		</div>
+
+		<div class="main2">
+			<div class="imgmain2">
+
+				<img src="images/faqba.png">
+
+			</div>
+
+			<div class="textomain2">
+				
+				<h2> Soy freelancer </h2>
+				
+				<h3> 1. Crea tu perfil  </h3>
+				<p> Completa toda la información que te resulte importante publicar para que otros puedan conocer tus trabajos realizados,  habilidades e intereses</p> 
+
+				<h3>2. Explora las oportunidades</h3>
+				<ul>
+					<li> Busca proyectos relacionados con tus intereses y habilidades</li> <br>
+					<li> Conversa en tiempo real</li><br>
+					<li> Acuerda entregables y tarifa por tu trabajo </li><br>
+				</ul>
+
+				<h3>3. ¡Taran... Ponte a trabaja! </h3>
+				<p> Esfuerzate por entregar el mejor trabajo, las personas que te contratan podrán valorar tus aportes a sus proyectos y así acumular increibles referencias. </p> 
+
+			</div>
+		</div>
+
+	
+			<h2 class="maintitulo3"> Preguntas Frecuentes </h2>
+			<div class="main3">
+				
+				
+				
+				<h3> 1. ¿Qué es shortcut?  </h3>
+				<p> Es una plataforma donde empleadores y una red global de profesionales independientes entran en contacto para trabajar en un objetivo en común bajo una contratación acordada entre las partes. Cualquier miembro puede publicar un proyecto, ya sea un trabajo a corto o largo plazo, y elegir entre profesionales independientes asi como cualquier freelancer puede explorar los proyectos publicados y ofrecer sus servicios.</p> 
+
+
+				<h3> 2. ¿Cómo me registro como freelancer?  </h3>
+				<p> Complete un breve formulario de registro. Se le pedirá que cree un nombre de usuario único para la identificación en nuestro sistema, proporcione una dirección de correo electrónico válida y confirme que ha leído nuestros Términos y condiciones. 
+
+				Confirme su dirección de correo electrónico. Cuando envíe el formulario de registro, le enviaremos un correo electrónico de verificación a la dirección de correo electrónico que proporcionó al registrarse. Siga el enlace proporcionado en el correo electrónico para activar su cuenta.
+
+				Una vez confirmado su correo electronico podrá completar el perfil con sus experiencias profesionales, sus intereses y habilidades claves. No olvides colocar una foto para que los empleadores, es mucho más fácil reconocer y relacionarse con una cara que con un nombre de usuario. 
+			
+				La información que proporcione en su página de perfil le dará a otros usuarios una visión general de sus habilidades y / o necesidades. Freelancer.com no comparte su información personal y de contacto en la página de detalles de su perfil </p>
+			
+				<h3> 3.  ¿Cómo me registro como empresa / lider de un proyecto?  </h3>
+
+				<p> Crea Tu Perfil. Complete su perfil de manera correcta y completa para que la comunidad de Freelancers sepa quién es usted como persona y lo que está buscando profesionalmente. Los usuarios con perfiles incompletos no podrán ofertar en proyectos.
+
+				Publica tus proyectos detallando el nombre, objetivo y descripción de lo que estás buscando en la comunidad de freelancers. Sé lo más especifico que puedas respecto a la experiencia y requisitos que necesitas de un freelancer.
+
+				Luego, entra en contacto con las personas que cumplan con tus requisitos para evaluar y seleccionar los mejores expertos en lo que buscas.</p> 	
+			
+				<h3> 4.  ¿Shorcut es quien me ofrece y me contrara?  </h3>
+
+				<p> Shorcut no ofrece ningún trabajo, pero te damos un sistema para encontrar trabajo. </p>
+
+				<h3> 5. ¿Puedo cambiar mi nombre de usuario?  </h3>
+
+				<p> No, el nombre de usuario es permanente, no podrá cambiarse. Por lo tanto, a la hora de elegir tu nombre de usuario, seleccione el más apropiado. </p>	
+
+
+				<h3> 6. ¿porque mi cuenta está suspendida?  </h3>
+
+				<p> Hay varias razones posibles para suspender la cuenta de un usuario:
+
+				<ul> 
+
+					<li> Publicar de información de contacto o enlaces a información de contacto en una descripción del proyecto, una oferta o en los tableros de mensajes. Esta es la razón más común para la eliminación del proyecto y la suspensión de la cuenta.
+					<li> Publicar contenido no válido o inadecuado en una descripción de proyecto, una oferta o en los tableros de mensajes. Esto puede incluir anuncios e información ilegal o inapropiada.</li>
+
+					<li> Publicar una solicitud para intercambiar fondos entre cuentas sin un proyecto existente. Shorcut no es un servicio de transferencia de dinero, y cualquier transacción de este tipo no está permitida.</li>
+
+					<li> Creando múltiples cuentas. Los miembros solo pueden tener UNA cuenta de Shorcut, que pueden usar tanto para contratar como para realizar ofertas.</li>
+
+					<li> Violar los Términos y Condiciones del Shorcut. Esto también puede resultar en la terminación de la cuenta. Lea y comprenda nuestros Términos y condiciones antes de usar el sitio. </li>
+					</ul>	
+
+
+				Si cree que su cuenta se ha suspendido por error, envíe un correo electrónico a support@shortcut.com utilizando su dirección de correo selectrónico registrada.</p>
+
+
+				<h3> 7. ¿Cómo califico y escribo una opinión sobre un freelancer?  </h3>
+
+				<p> Una vez que se completa un proyecto y se paga al profesional independiente el monto total, el sistema de comentarios para ese proyecto estará disponible. Se lo redireccionará automáticamente al formulario de comentarios una vez que marque su proyecto como completado. </p>	
+
+				<h3> 8. ¿Qué sucede si un Freelancer no completa mi proyecto?  </h3>
+
+				<p> Los empleadores tienen la opción de marcar su proyecto como incompleto si el profesional independiente no puede completar el trabajo requerido. El Informe Incompleto se publicará en la página de perfil del profesional independiente como una revisión del proyecto.
+
+				Para dejar un Informe Incompleto, vaya a la página del proyecto, haga clic en Finalizar Proyecto , seleccione "El profesional independiente no puede completar mi proyecto". y haz clic en Enviar.</p>
+				
+		</div>
+
+
+
+
+
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
