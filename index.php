80099f62f074a8e33263e85d455834e6990a2c20
anacg72 <ana.garbeleche@gmail.com>
2018-04-08 20:58:05 -0300

    Rami, fijate si ves los cambios de css

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="./css/indexStyle.css">
 	<title>SHORCUT</title>
@@ -9,6 +10,7 @@
 		<?php include 'header.php' ?>
 
 		<div class="main">
+
 			<h2 class="mainTittle">La plataforma donde los mejores freelancer e ideas se encuentran para dar vida a grandes proyectos.</h2>
 
 			<form action="./faq2.php">
